4593f652f2a63c32017a24386ff9366b04ccf271
juanpc19 <juan.perez@iesnervion.es>
2023-01-18 14:39:57 +0100

    ej1

--- Tema5-EjerciciosArraysEliminacion/src/ejercicios/Ejercicio1.java
+++ Tema5-EjerciciosArraysEliminacion/src/ejercicios/Ejercicio1.java
@@ -1,9 +1,34 @@
 package ejercicios;
 
+import java.util.Arrays;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int tabla[] = new int[20];
+		
+		int indiceBusqueda=0;
+
+		for (int i = 0; i < tabla.length; i++) {
+
+			tabla[i] = (int) (Math.random() * 100);
+		}
+
+		System.out.println(Arrays.toString(tabla));
+		
+		while (indiceBusqueda<tabla.length) {
+			{
+		
+				if (tabla[indiceBusqueda]%2!=0) {
+					System.arraycopy(tabla, indiceBusqueda+1, tabla, indiceBusqueda, tabla.length-indiceBusqueda-1);
+					tabla=Arrays.copyOf(tabla,tabla.length-1);
+				}
+			} 
+			indiceBusqueda++;
+		}
+		
+		System.out.println(Arrays.toString(tabla));
 
 	}
 
