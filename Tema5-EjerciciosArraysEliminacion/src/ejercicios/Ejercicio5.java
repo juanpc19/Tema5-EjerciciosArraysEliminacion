ccdb854828c382bc1d463dfba838560dc4f4c47a
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-01-21 18:16:02 +0100

    comentarios ej 5

--- Tema5-EjerciciosArraysEliminacion/src/ejercicios/Ejercicio5.java
+++ Tema5-EjerciciosArraysEliminacion/src/ejercicios/Ejercicio5.java
@@ -14,9 +14,7 @@ public class Ejercicio5 {
 		int posicion;// variable que guardara la posicion en la que se ha encontrado el numero
 		// introducido si este se ha encontrado
 
-		int posicionAleatoria1; // variable en la que guardare la posicion1 aleatoria a eliminar
-
-		int posicionAleatoria2; // variable en la que guardare la posicion2 aleatoria a eliminar
+		int posicionAleatoria; // variable en la que guardare la posicion1 aleatoria a eliminar
 
 		int numeroA; // variable en la que guardare el valor de la posicion1 del array tabla
 
@@ -30,7 +28,7 @@ public class Ejercicio5 {
 		System.out.println("Introduzca sus 10 numeros favoritos: ");
 
 		// bucle que se ejecutara mientras el array tabla tenga longitud inferior a 10
-		//para ir dando valores introducidos por el usuario al array tabla
+		// para ir dando valores introducidos por el usuario al array tabla
 		while (tabla.length < 10) {
 
 			// ordeno el array tabla al inicio de iteracion para busqueda binaria posterior
@@ -45,21 +43,21 @@ public class Ejercicio5 {
 
 			// compruebo si el numero introducido se encuentra en la tabla con binary search
 			// que me devolvera la posicion en la que lo ha encontrado si lo encuentra
-			// o una posicion negativa si no lo encuentra
+			// o una posicion de valor negativo si no lo encuentra
 			posicion = Arrays.binarySearch(tabla, numeroIntroducido);
 
-			// si valor de posicion menor a 0 no se encuentra en el array tabla
+			// si valor de posicion menor a 0 numeroIntroducido no se encuentra en el array tabla
 			if (posicion < 0) {
 
 				// por lo que creo una posicion en la que almacenarlo aumentando la longitud del
 				// array en +1
 				tabla = Arrays.copyOf(tabla, tabla.length + 1);
 
-				//y añado el numero introducido a la ultima posicion del array 
+				// y añado el numero introducido a la ultima posicion del array
 				tabla[tabla.length - 1] = numeroIntroducido;
 
-				// si valor de posicion mayor a 0 el numero introducido se encuentra en el array tabla
-				//por lo que muestro mensaje de error por pantalla
+				// si valor de posicion mayor a 0 numeroIntroducido se encuentra en el array tabla
+				// por lo que muestro mensaje de error por pantalla
 			} else {
 				System.out.println("Ya ha introducido ese numero, introduzca uno que no este en la lista. ");
 			}
@@ -68,42 +66,65 @@ public class Ejercicio5 {
 
 		dogma.close();
 
-		//print de array tabla al final de bucle para comprobar funcionamiento
-		System.out.println(Arrays.toString(tabla));
+		// mensaje por pantalla de finalizacion de creacion de array mostrando el mismo
+		System.out.println("Array creado: " + Arrays.toString(tabla));
 
-		//mensaje por pantalla de finalizacion de creacion de array
-		System.out.println("Array creado. ");
+		System.out.println();
 
-		//bucle que se ejecutara mientras la longitud del array tabla sea mayor a 1
+		// bucle que se ejecutara mientras la longitud del array tabla sea mayor a 1
 		while (tabla.length > 1) {
 
-			//
-			posicionAleatoria1 = (int) (Math.random() * tabla.length);
-			numeroA = tabla[posicionAleatoria1];
+			// doy valor aleatorio desde 0 a longitud actual del array a posicionAleatoria
+			// con math random
+			posicionAleatoria = (int) (Math.random() * tabla.length);
 
-			System.arraycopy(tabla, posicionAleatoria1 + 1, tabla, posicionAleatoria1,
-					tabla.length - posicionAleatoria1 - 1);
+			// doy valor a numeroA con valor del elemento contenido en posicion del array
+			// tabla posicionAleatoria
+			numeroA = tabla[posicionAleatoria];
+
+			// copio el array tabla desde la posicion siguiente a la posicionAleatoria sobre
+			// si mismo por una longitud igual a la de la tabla menos la posicionAleatoria -1
+			// eliminando la posicionAleatoria y dejando la ultima con valor por defecto 0
+			System.arraycopy(tabla, posicionAleatoria + 1, tabla, posicionAleatoria,
+					tabla.length - posicionAleatoria - 1);
+			// copio el array eliminando la ultima posicion con tabla.length-1
 			tabla = Arrays.copyOf(tabla, tabla.length - 1);
 
 			if (tabla.length == 1) {
 				break;
 			}
 
-			posicionAleatoria2 = (int) (Math.random() * tabla.length);
-			numeroB = tabla[posicionAleatoria2];
+			// doy valor aleatorio desde 0 a longitud actual del array a posicionAleatoria
+			// con math random
+			posicionAleatoria = (int) (Math.random() * tabla.length);
 
-			System.arraycopy(tabla, posicionAleatoria2 + 1, tabla, posicionAleatoria2,
-					tabla.length - posicionAleatoria2 - 1);
+			// doy valor a numeroB con valor del elemento contenido en posicion del array
+			// tabla posicionAleatoria
+			numeroB = tabla[posicionAleatoria];
 
+			// copio el array tabla desde la posicion siguiente a la posicionAleatoria sobre
+			// si mismo por una longitud igual a la de la tabla menos la posicionAleatoria -1
+			// eliminando la posicionAleatoria y dejando la ultima con valor por defecto 0
+			System.arraycopy(tabla, posicionAleatoria + 1, tabla, posicionAleatoria,
+					tabla.length - posicionAleatoria - 1);
+
+			// doy valor a mediaAritmetica igual a la media aritmetica de numeroA y numeroB
 			mediaAritmetica = (int) ((numeroA + numeroB) / 2);
 
+			// introduzco valor de mediaAritmetica en la ultima posicion del array en lugar
+			// machacandola en lugar de borrarla como con la posicion anterior
 			tabla[tabla.length - 1] = mediaAritmetica;
 
+			// print de array tabla para comprobacion en pantalla
 			System.out.println(Arrays.toString(tabla));
 		}
 
+		// print del resultado final del array tabla para comprobacion de funcionamiento
 		System.out.println(Arrays.toString(tabla));
 
+		System.out.println();
+
+		// print a usuario con su numero de la suerte
 		System.out.println("Su numero de la suerte es: " + (tabla[0]));
 
 	}
